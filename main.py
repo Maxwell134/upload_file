fbf718f918332ade9542a90e5b8477080fc78ebf
7002370412 <tridibbasumatary@Tridibs-MacBook-Air.local>
2023-01-12 21:59:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask ,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/search", methods=["POST", "GET"])
+def search():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return f"<h1>{user}</h1>"
+    else:
+        return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
